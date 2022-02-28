fab62bdd2c8d8b0206b5e3ab05a2d98d35f4ec16
FunnyMan11 <kobuladze.nk1@gmail.com>
2022-02-28 16:57:35 +0400

    restaurant-application
    Made with react v.1.0

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+      </div>
+      <p> &copy; 2021 georgianfood.com</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/helpers/MenuList.js
@@ -0,0 +1,72 @@
+import khinkali from "../assets/khinkali.jpg";
+import khachapuri from "../assets/khachapuri.jpg";
+import kubdari from "../assets/kubdari.jpg";
+import chakhokhbili from "../assets/chakhokhbili.jpg";
+import chaqafuli from "../assets/chaqafuli.jpg";
+import ghomi from "../assets/ghomi.png";
+import lobiani from "../assets/lobiani.jpg";
+import ostri from "../assets/ostri.jpg";
+import sacivi from "../assets/sacivi.jpg";
+import tatara from "../assets/tatara.jpg";
+import chixirtma from "../assets/chixirtma.jpg";
+
+
+
+
+export const MenuList = [
+  {
+    name: "Khinkali",
+    image: khinkali,
+    price: 250,
+  },
+  {
+    name: "Khachapuri",
+    image: khachapuri,
+    price: 280,
+  },
+  {
+    name: "Kubdari",
+    image: kubdari,
+    price: 350,
+  },
+  {
+    name: "chakhokhbili",
+    image: chakhokhbili,
+    price: 230,
+  },
+  {
+    name: "chaqafuli",
+    image: chaqafuli,
+    price: 450,
+  },
+  {
+    name: "ghomi",
+    image: ghomi,
+    price: 499,
+  },
+  {
+    name: "lobiani",
+    image: lobiani,
+    price: 499,
+  },
+  {
+    name: "ostri",
+    image: ostri,
+    price: 499,
+  },
+  {
+    name: "sacivi",
+    image: sacivi,
+    price: 499,
+  },
+  {
+    name: "tatara",
+    image: tatara,
+    price: 499,
+  },
+  {
+    name: "chixirtma",
+    image: chixirtma,
+    price: 499,
+  },
+];
